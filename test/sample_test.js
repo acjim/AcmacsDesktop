9942e8a9f79cc0921eaeb9c7275e578fdce3e10b
Roan <rohankoid@gmail.com>
2015-09-08 10:51:35 +1000

    sample unit test added #51, issue with testing angular modules #51 @todo further work required

---
+++ test/sample_test.js
@@ -0,0 +1,20 @@
+/*
+USAGE:
+To run the test cases, on command line:
+ $ node_modules/nodeunit/bin/nodeunit test/sample_test.js
+ */
+var api = require('../app/shared/api.js');
+
+exports.create_file = function (test) {
+    test.expect(1); // number of expected tests
+    var file_name = '../test/data/concentric-circles.acd1';
+    var expected_name = 'concentric-circles.json';
+    test.equals(api.createMapFile(file_name), expected_name);
+    test.done();
+
+};
+
+exports.test_name = function (test) {
+    test.equals(true, true);
+    test.done();
+};
