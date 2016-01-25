30a0995b7a5af6a9802ad04971b0488e9b1681ce
idrissou <idriss.benarafa@gmail.com>
2016-01-25 19:02:01 +0100

    Fixing erro lines new bug

--- src/app/components/map/mapDirective.js
+++ src/app/components/map/mapDirective.js
@@ -96,21 +96,27 @@ app.directive('d3Map', ['$rootScope', '$window', '$timeout', 'toolbar', 'toolbar
                     .on("zoom", applyZoom);
 
                 // reapply scales on the data
-                nodeGroup.attr("transform", function (d) {
-                    return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
-                });
-                labelsGroup.attr("transform", function (d) {
-                    return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
-                });
+                if (nodeGroup) {
+                    nodeGroup.attr("transform", function (d) {
+                        return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
+                    });
 
-                // Create brush
-                brush = createBrush();
+                    if (labelsGroup) {
+                        labelsGroup.attr("transform", function (d) {
+                            return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
+                        });
+                    }
+                    // Create brush
+                    brush = createBrush();
 
-                // Create background grid
-                boxGroup = redrawGrid(boxGroup, boxSize, width / minimalScaleValue, height / minimalScaleValue);
+                    // Create background grid
+                    if (boxGroup)
+                        boxGroup = redrawGrid(boxGroup, boxSize, width / minimalScaleValue, height / minimalScaleValue);
 
-                manageMapTools();
-                applyZoom();
+
+                    manageMapTools();
+                    applyZoom();
+                }
             }
 
             /**
